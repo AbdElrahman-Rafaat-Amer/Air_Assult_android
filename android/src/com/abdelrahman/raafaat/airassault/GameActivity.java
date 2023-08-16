eef934425b7e275c08bee274ea9cff819788d5a1
Abdelrahman Rafaat <abdelrahman.raafaat@gmail.com>
2023-08-16 23:47:52 +0200

    [AA][Android] Add Game Scene
    [AA][Android] Add Game Scene
    https://abdelrahmanraafat.atlassian.net/browse/AA-10
    
    Work Done:
    ========
    - Use viewBinding instead of findViewByID.
    - Create Game Fragment.
    - Make application only in portrait mode.
    -

--- android/src/com/abdelrahman/raafaat/airassault/GameActivity.java
+++ android/src/com/abdelrahman/raafaat/airassault/GameActivity.java
@@ -2,17 +2,33 @@ package com.abdelrahman.raafaat.airassault;
 
 import android.os.Bundle;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class GameActivity extends AndroidApplication {
+import com.abdelrahman.raafaat.airassault.databinding.ActivityGameBinding;
 
-    private AirAssault gameScene = new AirAssault();
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(gameScene, config);
+
+        ActivityGameBinding binding = ActivityGameBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        GameFragment gameFragment;
+        FragmentManager manager = getSupportFragmentManager();
+        if (savedInstanceState == null) {
+            gameFragment = new GameFragment();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.setReorderingAllowed(true);
+            transaction.add(R.id.fragment_container, gameFragment, "GAME_FRAGMENT");
+            transaction.commit();
+        } else {
+            gameFragment = (GameFragment) manager.findFragmentByTag("GAME_FRAGMENT");
+        }
+
     }
 }
